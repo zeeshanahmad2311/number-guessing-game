940e88f3962ad120652a67d3179cf31a334be820
zeeshan ahmad <152972620+zeeshanahmad2311@users.noreply.github.com>
2024-05-31 12:41:15 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log(randomNumber);
+const answer = await inquirer.prompt([{
+        name: "userGussNumber",
+        type: "number",
+        message: " please guess a number between 1-10 :",
+    },]);
+if (answer.userGussNumber === randomNumber) {
+    console.log("congrates ypu guess the right number");
+}
+else {
+    console.log("you guessed the wrong number");
+}
